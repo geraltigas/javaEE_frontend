685128f70b5c3b3963b1f513e5fe55b623becc15
lzy <1004948150@qq.com>
2022-12-15 03:50:43 +0800

    update

---
+++ src/pages/projects/ProjectList.vue
@@ -0,0 +1,57 @@
+<template>
+<el-row class="wrapper">
+    <el-col :xs="24" :sm="24" :xl="24" style="margin: 0 auto;">
+      <el-col v-for="project in projects.records" :key="project.idProject" style="padding-bottom: 1rem;">
+        <project-item :item="project"></project-item>
+      </el-col>
+      <el-col>
+        <div class="vertical-container text-center">
+          <el-pagination class="page"
+                         :hide-on-single-page="true"
+                         layout="total, prev, pager, next, jumper"
+                         :page-size="projects.size"
+                         :current-page="projects.current"
+                         :total="projects.total"
+                         prev-text="上一页"
+                         next-text="下一页"
+                         @current-change="currentChange">
+          </el-pagination>
+        </div>
+      </el-col>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import ProjectItem from '@/pages/projects/ProjectItem.vue'
+export default {
+    name: 'ProjectList',
+    props: {
+        projects: {
+            type: Object
+        }
+    },
+    methods: {
+        currentChange(page) {
+            this.$emit('currentChange', page)
+        }
+    },
+    components: {
+        ProjectItem
+    }
+}
+</script>
+<style scoped>
+.wrapper {
+  max-width: 980px;
+  margin: 20px auto;
+  display: block;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  box-sizing: border-box;
+  float: none;
+}
+.page {
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
---
+++ src/pages/videos/VideoList.vue
@@ -0,0 +1,58 @@
+<template>
+    <el-row class="wrapper">
+        <el-col :xs="24" :sm="24" :xl="24" style="margin: 0 auto;">
+          <el-col v-for="video in videos.records" :key="video.idVideo" style="padding-bottom: 1rem;">
+            <video-item :item="video"></video-item>
+          </el-col>
+          <el-col>
+            <div class="vertical-container text-center">
+              <el-pagination class="page"
+                             :hide-on-single-page="true"
+                             layout="total, prev, pager, next, jumper"
+                             :page-size="videos.size"
+                             :current-page="videos.current"
+                             :total="videos.total"
+                             prev-text="上一页"
+                             next-text="下一页"
+                             @current-change="currentChange">
+              </el-pagination>
+            </div>
+          </el-col>
+        </el-col>
+      </el-row>
+    </template>
+    <script>
+    import VideoItem from '@/pages/videos/VideoItem.vue'
+    export default {
+        name: 'VideoList',
+        props: {
+            videos: {
+                type: Object
+            }
+        },
+        methods: {
+            currentChange(page) {
+                console.log("videolist "+page);
+                this.$emit('currentChange', page)
+            }
+        },
+        components: {
+            VideoItem
+        }
+    }
+    </script>
+    <style scoped>
+    .wrapper {
+      max-width: 980px;
+      margin: 20px auto;
+      display: block;
+      padding-left: 1rem;
+      padding-right: 1rem;
+      box-sizing: border-box;
+      float: none;
+    }
+    .page {
+      margin-top: 20px;
+      text-align: center;
+    }
+    </style>
