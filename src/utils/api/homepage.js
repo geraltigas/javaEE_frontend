0ee39483721076d841577b035c1cf4d1d9a3fa4f
lzy <1004948150@qq.com>
2022-12-15 02:56:52 +0800

    update

---
+++ src/utils/api/homepage.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+export const GET_VIDEO = (data) => {
+    return request('get', `/videos`,data)
+}
+
+export const GET_PROJECT = (data) => {
+    return request('get', `/projects`,data)
+}
+
+export const GET_KNOWLEDGE = (data) => {
+    return request('get','mysql-knowledge/top',data)
+}
+
+export const GET_NOTE = (data) => {
+    return request("get","/get-notes-by-time",data)
+}
