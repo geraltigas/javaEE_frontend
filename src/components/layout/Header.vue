2f03c5eddee5c362ac96b142874d065e1dd013e8
王嘉融 <1436694994@qq.com>
2022-12-14 15:11:35 +0800

    final.7

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -31,6 +31,33 @@
 
     <el-col :md="10" :span="10" :xs="16" style="line-height: 60px">
         <el-col style="text-align: right;" v-if="loggedIn">
+          <el-popover
+            placement="bottom"
+            trigger="click"
+            v-model="showPopover"
+            width="400">
+            <el-input @keyup.enter.native="querySearchAsync" name="searchInput" placeholder="搜索"
+                      v-model="queryString">
+              <el-button @click="querySearchAsync" slot="append">
+                <svg height="24" style="fill: rgba(0, 0, 0, 1);" viewBox="0 0 24 24" width="24"
+                    xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+                  <path
+                    d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path>
+                </svg>
+              </el-button>
+            </el-input>
+            <el-button circle size="small" slot="reference">
+              <svg height="14" style="fill: rgba(0, 0, 0, 1);" viewBox="0 0 24 24" width="14"
+                  xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
+                <path
+                  d="M11.412 8.586c.379.38.588.882.588 1.414h2a3.977 3.977 0 0 0-1.174-2.828c-1.514-1.512-4.139-1.512-5.652 0l1.412 1.416c.76-.758 2.07-.756 2.826-.002z"></path>
+              </svg>
+            </el-button>
+          </el-popover>
           <el-link :underline="false" rel="nofollow" style="margin-left: 10px;">
             <el-dropdown   @command="handleCommand" trigger="click">
               <el-avatar size="small" src="https://static.rymcu.com/article/1578475481946.png"></el-avatar>
@@ -39,7 +66,7 @@
                   <el-avatar class="mr-3" size="small" src="https://static.rymcu.com/article/1578475481946.png"
                             style="margin-top: 1rem;"></el-avatar>
                   <el-link :underline="false" rel="nofollow" style="margin-left: 10px;margin-bottom: 1rem;">
-                    {{ user.nickname }}
+                    {{ user.username }}
                   </el-link>
                 </el-dropdown-item>
                 <el-dropdown-item command="mypage" divided>我的主页</el-dropdown-item>
@@ -108,7 +135,7 @@ export default {
       this.getUser()
     }
     else{
-      this.loggedIn = false
+      this.loggedIn = true
     }
   },
   methods:{
